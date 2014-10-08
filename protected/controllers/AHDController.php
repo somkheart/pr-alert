cac04a1406ee1a36abd67191ca2efe52482291f2
somkheart <engineer_robot@hotmail.com>
2014-10-08 18:47:22 +0700

    test Commit

---
+++ protected/controllers/AHDController.php
@@ -0,0 +1,61 @@
+<?php
+
+class AHDController extends  CAHDController
+{
+	public function actionIndex()
+	{
+		$this->render('index');
+	}
+        /*
+        public function filters()
+	{
+		return array(
+			'accessControl', // perform access control for CRUD operations
+			//'postOnly + delete', // we only allow deletion via POST request
+		);
+	}
+	/**
+	 * Specifies the access control rules.
+	 * This method is used by the 'accessControl' filter.
+	 * @return array access control rules
+	 */
+	public function accessRules()
+	{
+		return array(
+			array('allow',  // allow all users to perform 'index' and 'view' actions
+				'actions'=>array('index','view'),
+				'users'=>array('@'),
+			)
+		);
+	}
+        public function actionChoice()
+        {
+            $this->render('Choice');
+        }
+	// Uncomment the following methods and override them if needed
+	/*
+	public function filters()
+	{
+		// return the filter configuration for this controller, e.g.:
+		return array(
+			'inlineFilterName',
+			array(
+				'class'=>'path.to.FilterClass',
+				'propertyName'=>'propertyValue',
+			),
+		);
+	}
+
+	public function actions()
+	{
+		// return external action classes, e.g.:
+		return array(
+			'action1'=>'path.to.ActionClass',
+			'action2'=>array(
+				'class'=>'path.to.AnotherActionClass',
+				'propertyName'=>'propertyValue',
+			),
+		);
+	}
+	*/
+}
